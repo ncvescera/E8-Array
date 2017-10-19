28052a51330ccdbb0a7810de654d74925f1db267
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-19 22:46:50 +0200

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,55 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 19 ottobre 2017, 22.29
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "bool.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define DIM 4
 
-    return (EXIT_SUCCESS);
+/**
+* Funzione che stabilisce se la matrice passata è diagonale
+* @param matrix Matrice
+* @param dim Dimenzioe della matrice
+* @return 1 se la matrice è diagonale, 0 se non lo è
+*/
+bool diagonale(int matrix[DIM][DIM], int dim);
+
+int main(){
+  //dichiarazione matrice quadrata
+  int matrix[DIM][DIM] = {
+    {9,0,0,0},
+    {0,9,0,0},
+    {0,0,9,0},
+    {0,0,0,9}
+  };
+  bool result;
+
+  //stampo la matrice
+  for(int i = 0; i < DIM; i++){
+    for(int j = 0; j < DIM; j++)
+      printf("%d ",matrix[i][j]);
+
+    printf("\n");
+  }
+
+  result = diagonale(matrix,DIM);
+
+  if(result)
+    printf("La matrice e' diagonale\n");
+  else
+    printf("La matrice NON e' diagonale\n");
+
+  return(EXIT_SUCCESS);
 }
 
+bool diagonale(int matrix[DIM][DIM], int dim){
+  bool result = TRUE;
+
+  for(int righe = 0; righe < DIM && result; righe++){
+    for(int colonne = 0; colonne < DIM && result; colonne++)
+      if(righe != colonne){
+        if(matrix[righe][colonne] != 0)
+          result = FALSE;
+      }
+  }
+
+  return result;
+}
