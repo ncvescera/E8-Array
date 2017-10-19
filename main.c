56ed7d923bb6c836c906772305bc145cd746d5e0
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-19 22:35:27 +0200

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 19 ottobre 2017, 22.29
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "dimOf.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+/**
+* Funzione che dato un array e la sua dimenzione restituisce la media dei suoi elementi
+* @param array Array
+* @param dim Dimenzione dell'array
+* @return media degli elementi dell'array
+*/
+double media(double array[], int dim);
 
-    return (EXIT_SUCCESS);
+int main(){
+  int result;
+  double array[] = {2,4,6};  //array contenente i valori da elaborare
+
+  result = media(array, sizeOfArray(array));
+
+  printf("La media è: %d\n",result);
+
+  return (EXIT_SUCCESS);
 }
 
+double media(double array[], int dim){
+  double media = 0;
+  int i;
+
+  for(i = 0; i < dim; i++)
+      media += array[i];
+
+  media /= i;
+
+  return media;
+}
